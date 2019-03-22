c2728a33074a8a77f87da3fb0b9d5f364a939999
Jorgen <scubajorgen@gmail.com>
2019-03-22 21:07:09 +0100

    Added some illustrative examples

--- src/main/java/net/studioblueplanet/enigma/EnigmaSimulator.java
+++ src/main/java/net/studioblueplanet/enigma/EnigmaSimulator.java
@@ -5,23 +5,32 @@
  */
 package net.studioblueplanet.enigma;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author jorgen
  */
 public class EnigmaSimulator
 {
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args)
+    private static String exampleCypher=
+                        "MLETW NNJZJ ECTHJ JUUVD DBAZS"+
+                        "XBVOF SEWLR MLSBX GNLNL LLIGO"+
+                        "IMYMU HBGRI HZTFG TXKCT CRPJN"+
+                        "BHZMQ QWGUA KBJXJ BITQI BFXMZ"+
+                        "MHHWK OBSSD OGELQ UWQXG IMLVG"+
+                        "YJQSL PIQNZ VQVFN LGQXN PUJEZ"+
+                        "MPUNC MLETS JIXZP HUNBN AGVCB"+
+                        "SAAIN IIHJV CWISA NHEMW JOEIY"+
+                        "MXSFE JAEPU FJLIT VUZYS HYWYP"+
+                        "XTAYJ CARWI YORFI VANOM BXWDT";            
+
+    private static void simpleExample()
     {
         Enigma  enigma;
-        String  text;
-        String  encrypted;
+        String  plaintext;
+        String  cyphertext;
         
-       
         enigma=new EnigmaM3();
         enigma.setWalzen("I II III");
         enigma.setUmkehrWalze("UKW B");
@@ -29,24 +38,90 @@ public class EnigmaSimulator
         enigma.setRingStellungen("1 2 3");
         enigma.setGrundStellungen("Q D U");
 
-        text="Die Enigma ist eine Rotor-Schlusselmaschine, die im Zweiten Weltkrieg verwendet wurde";
+        plaintext="Die Enigma ist eine Rotor-Schlusselmaschine, die im Zweiten Weltkrieg verwendet wurde";
 
         System.out.println("ENCRYPT");
         System.out.println("Grundstellung : "+enigma.getGrundStellungen());
-        encrypted=enigma.encodeDecode(text);
-        System.out.println("Text to ecrypt: "+text);
-        System.out.println("Encrypted     : "+encrypted);
+        cyphertext=enigma.encodeDecode(plaintext);
+        System.out.println("Text to ecrypt: "+plaintext);
+        System.out.println("Encrypted     : "+cyphertext);
         System.out.println("Grundstellung : "+enigma.getGrundStellungen());
         
         
         System.out.println("DECRYPT");
         enigma.setGrundStellungen("Q D U");
         System.out.println("Grundstellung : "+enigma.getGrundStellungen());
-        text=enigma.encodeDecode(encrypted);
-        System.out.println("Decrypted     : "+text);
+        plaintext=enigma.encodeDecode(cyphertext);
+        System.out.println("Decrypted     : "+plaintext);
         System.out.println("Grundstellung : "+enigma.getGrundStellungen());
+    }
+    
+    private static void findWaltzen()
+    {
+        Enigma                  enigma;
+        String                  decoded;
+        Integer[]               permutation;
+        ArrayList<Integer[]>    permutations;
+        int                     i;
+        boolean                 found;
+
+        int             limit;
+        int             count;
+
+        String[]        waltzen       ={"I", "II", "III", "IV", "V"};
+        int[]           permElements     ={0, 1, 2, 3, 4};
+
+        enigma=new EnigmaM3(); 
+
+        enigma.setSteckers("au cm dp ev hl iz jw no qx st");
+        enigma.setUmkehrWalze("UKW B");
+
+
+        permutations=new ArrayList<Integer[]>();
+        Toolbox.permute(permutations, permElements, 3, 0);
         
-  
+        limit=exampleCypher.length()*9/100;
+
+        found=false;
+        i=0;
+        while ((i<permutations.size()) && !found)
+        {
+            permutation=permutations.get(i);
+
+            System.out.println("Permuation: "+permutation[0]+" "+permutation[1]+" "+permutation[2]+
+                               ": "+waltzen[permutation[0]]+" "+waltzen[permutation[1]]+" "+waltzen[permutation[2]]);
+
+            enigma.setWalzen(waltzen[permutation[0]]+" "+waltzen[permutation[1]]+" "+waltzen[permutation[2]]);
+            
+
+            enigma.setRingStellungen("05 22 11");    
+            enigma.setGrundStellungen("U I O");
+            
+            decoded=enigma.encodeDecode(exampleCypher);
+            
+            count=Toolbox.countCharacter(decoded, 'E');
+            if (count>limit)
+            {
+                System.out.println("Found: "+decoded);
+                found=true;
+            }   
+            i++;
+        }
+
+     }
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("############## SIMPLE EXAMPLE ######################");
+        simpleExample();
+        
+        System.out.println();
+        System.out.println("############## FIND WALTZEN ########################");
+        findWaltzen();
     }
 
     
